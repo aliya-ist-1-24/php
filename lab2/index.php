830ba1bbde9598663c11e8cdb5d71abc100da9df
Djaparova Aliya <aliyadjaparova006@gmail.com>
2026-02-14 10:00:00 +0600

    lab2

---
+++ lab2/index.php
@@ -0,0 +1,62 @@
+<?php
+$name1="Askarbekova Malika"; $age1="19"; $group1="IST-1-24";
+$name2="Japarova Aliya"; $age2="19"; $group2="IST-1-24";
+$name3="Kubanychbekova Elina"; $age3="19"; $group3="IST-1-24";
+$name4="Borubeeva Sheker"; $age4="18"; $group4="IST-1-24";
+$name5="Shadybekova Nazgul"; $age5="20"; $group5="IST-1-24";
+$name6="Ysak Emir"; $age6="20"; $group6="IST-1-24";
+$name7="Edilbekova Sanirabiga"; $age7="19"; $group7="IST-1-24";
+$name8="Aidarova Aidana"; $age8="19"; $group8="IST-1-24";
+$name9="Kanatbekova Guldana"; $age9="19"; $group9="IST-1-24";
+$name10="Kydyrbekov Kanimet"; $age10="19"; $group10="IST-1-24";
+$name11="Kubanychbek uulu Elmir"; $age11="19"; $group11="IST-1-24";
+$name12="Kadyrbekov Farhat"; $age12="19"; $group12="IST-1-24";
+$name13="Toktoshev Nurbol"; $age13="20"; $group13="IST-1-24";
+$name14="Akbai kyzy Aizada"; $age14="19"; $group14="IST-1-24";
+$name15="Sharipova Jibek"; $age15="18"; $group15="IST-1-24";
+$name16="Mavlyanova Aidana"; $age16="19"; $group16="IST-1-24";
+$name17="Masalbekova Shirin"; $age17="19"; $group17="IST-1-24";
+$name18="Azimov Temirlan"; $age18="20"; $group18="IST-1-24";
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Students</title>
+</head>
+<body>
+
+<table border="1" cellpadding="8">
+<tr>
+<th>ФИО</th>
+<th>Возраст</th>
+<th>Группа</th>
+</tr>
+
+<tr><td><?php echo $name1; ?></td><td><?php echo $age1; ?></td><td><?php echo $group1; ?></td></tr>
+<tr><td><?php echo $name2; ?></td><td><?php echo $age2; ?></td><td><?php echo $group2; ?></td></tr>
+<tr><td><?php echo $name3; ?></td><td><?php echo $age3; ?></td><td><?php echo $group3; ?></td></tr>
+<tr><td><?php echo $name4; ?></td><td><?php echo $age4; ?></td><td><?php echo $group4; ?></td></tr>
+<tr><td><?php echo $name5; ?></td><td><?php echo $age5; ?></td><td><?php echo $group5; ?></td></tr>
+<tr><td><?php echo $name6; ?></td><td><?php echo $age6; ?></td><td><?php echo $group6; ?></td></tr>
+<tr><td><?php echo $name7; ?></td><td><?php echo $age7; ?></td><td><?php echo $group7; ?></td></tr>
+<tr><td><?php echo $name8; ?></td><td><?php echo $age8; ?></td><td><?php echo $group8; ?></td></tr>
+<tr><td><?php echo $name9; ?></td><td><?php echo $age9; ?></td><td><?php echo $group9; ?></td></tr>
+<tr><td><?php echo $name10; ?></td><td><?php echo $age10; ?></td><td><?php echo $group10; ?></td></tr>
+<tr><td><?php echo $name11; ?></td><td><?php echo $age11; ?></td><td><?php echo $group11; ?></td></tr>
+<tr><td><?php echo $name12; ?></td><td><?php echo $age12; ?></td><td><?php echo $group12; ?></td></tr>
+<tr><td><?php echo $name13; ?></td><td><?php echo $age13; ?></td><td><?php echo $group13; ?></td></tr>
+<tr><td><?php echo $name14; ?></td><td><?php echo $age14; ?></td><td><?php echo $group14; ?></td></tr>
+<tr><td><?php echo $name15; ?></td><td><?php echo $age15; ?></td><td><?php echo $group15; ?></td></tr>
+<tr><td><?php echo $name16; ?></td><td><?php echo $age16; ?></td><td><?php echo $group16; ?></td></tr>
+<tr><td><?php echo $name17; ?></td><td><?php echo $age17; ?></td><td><?php echo $group17; ?></td></tr>
+<tr><td><?php echo $name18; ?></td><td><?php echo $age18; ?></td><td><?php echo $group18; ?></td></tr>
+
+</table>
+
+</body>
+</html>
